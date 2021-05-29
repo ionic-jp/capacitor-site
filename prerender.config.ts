013ec2783b5fc48a27bb55be25b2b5b2367fb628
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2021-05-29 17:33:56 +0900

    chore(hydrate): タイムアウトを倍に変更

--- prerender.config.ts
+++ prerender.config.ts
@@ -3,12 +3,12 @@ import { PrerenderConfig } from '@stencil/core';
 export const config: PrerenderConfig = {
   hydrateOptions() {
     return {
-      timeout: 30000,
+      timeout: 60000,
     };
   },
   filterUrl(url) {
     if (url.pathname === '/docs/pwa-elements' || url.pathname === '/docs/v3/pwa-elements' || url.pathname === '/docs/v2/pwa-elements') {
-      // gets a redirect in prod  
+      // gets a redirect in prod
       return false;
     }
     return true;
